e316dd24f35d204af00425db6c18e3987d1d48d2
Muhammad Nuzaihan Bin Kamal Luddin <muhammad@taqisystems.com>
2024-02-04 02:06:14 +0800

    WIP

---
+++ cmd/go-runner/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+
+
+        _ "firetail_apisix_plugin/cmd/go-runner/plugins"
+	"github.com/apache/apisix-go-plugin-runner/pkg/runner"
+	"github.com/apache/apisix-go-plugin-runner/pkg/log"
+)
+
+const (
+	LogFilePath     = "./logs/runner.log"
+)
+
+func openFileToWrite(name string) (*os.File, error) {
+	dir := filepath.Dir(name)
+	if dir != "." {
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			return nil, err
+		}
+	}
+	f, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+func newRunCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "run",
+		Short: "run",
+		Run: func(cmd *cobra.Command, _ []string) {
+			cfg := runner.RunnerConfig{}
+			f, err := openFileToWrite(LogFilePath)
+			if err != nil {
+				log.Fatalf("failed to open log: %s", err)
+			}
+			cfg.LogOutput = f
+
+			runner.Run(cfg)
+		},
+	}
+	return cmd
+}
+
+func NewCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "go-runner [command]",
+		Long:    "The Plugin runner to run Firetail Go plugins",
+		Version: "0.0.1",
+	}
+
+	cmd.AddCommand(newRunCommand())
+	return cmd
+}
+
+func main() {
+	root := NewCommand()
+	if err := root.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
---
+++ cmd/go-runner/plugins/firetail.go
@@ -0,0 +1,86 @@
+package plugins
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"io"
+
+	pkgHTTP "github.com/apache/apisix-go-plugin-runner/pkg/http"
+	"github.com/apache/apisix-go-plugin-runner/pkg/log"
+	"github.com/apache/apisix-go-plugin-runner/pkg/plugin"
+	"bytes"
+
+	firetail "github.com/FireTail-io/firetail-go-lib/middlewares/http"
+)
+
+type Firetail struct {
+	plugin.DefaultPlugin
+}
+
+type FiretailConf struct {
+  Body string `json:"body"`
+}
+
+func (p *Firetail) Name() string {
+  return "firetail"
+}
+
+func (p *Firetail) ParseConf(in []byte) (interface{}, error) {
+        conf := FiretailConf{}
+        err := json.Unmarshal(in, &conf)
+        return conf, err
+}
+
+func (p *Firetail) Filter(conf interface{}, res http.ResponseWriter, req pkgHTTP.Request) {
+	firetailMiddleware, err := firetail.GetMiddleware(&firetail.Options{
+		OpenapiSpecPath:          "./appspec.yml",
+		LogsApiToken:             "",
+		LogsApiUrl:               "",
+		DebugErrs:                true,
+		EnableRequestValidation:  false,
+		EnableResponseValidation: true,
+	})
+
+	if err != nil {
+		log.Errorf("Failed to initialise Firetail middleware, err:", err.Error())
+	}
+
+	// Create a fake handler
+	placeholderResponse := []byte{}
+	myHandler := &stubHandler{
+		responseCode:  200,
+		responseBytes: placeholderResponse,
+	}
+
+	// Create our middleware instance with the stub handler
+	myMiddleware := firetailMiddleware(myHandler)
+
+	localResponseWriter := httptest.NewRecorder()
+
+	body, err := req.Body()
+	if err != nil {
+                log.Errorf("cannot get body:", err.Error())
+	}
+
+	// Serve the request to the middlware
+	myMiddleware.ServeHTTP(localResponseWriter, httptest.NewRequest(
+		req.Method(), string(req.Path()),
+		io.NopCloser(bytes.NewBuffer(body)),
+	))
+
+	middlewareResponseBodyBytes, err := io.ReadAll(localResponseWriter.Body)
+
+	res.Header().Add("X-Resp-A6-Runner", "Go")
+	_, err = res.Write(middlewareResponseBodyBytes)
+	if err != nil {
+		log.Errorf("failed to write %s", err)
+	}
+}
+
+func init() {
+  err := plugin.RegisterPlugin(&Firetail{})
+  if err != nil {
+    log.Fatalf("failed to register plugin firetail: %s", err)
+  }
+}
