f868200fe39e9f9f524d49df7a5fdd50e8e44fc1
Muhammad Nuzaihan Bin Kamal Luddin <muhammad@taqisystems.com>
2024-02-04 03:43:45 +0800

    add response validation code

--- cmd/go-runner/plugins/firetail.go
+++ cmd/go-runner/plugins/firetail.go
@@ -71,10 +71,89 @@ func (p *Firetail) RequestFilter(conf interface{}, res http.ResponseWriter, req 
 
 	middlewareResponseBodyBytes, err := io.ReadAll(localResponseWriter.Body)
 
-	res.Header().Add("X-Resp-A6-Runner", "Go")
-	_, err = res.Write(middlewareResponseBodyBytes)
 	if err != nil {
-		log.Errorf("failed to write %s", err)
+		log.Errorf("Failed to read request body bytes from middleware, err: ", err.Error())
+
+                res.Header().Add("X-Resp-A6-Runner", "Go")
+                _, err = res.Write(middlewareResponseBodyBytes)
+                if err != nil {
+                        log.Errorf("failed to write %s", err)
+                }
+	}
+
+	if string(middlewareResponseBodyBytes) != string(placeholderResponse) {
+		log.Errorf("Middleware altered response body, original: %s, new: %s", string(placeholderResponse), string(middlewareResponseBodyBytes))
+
+                res.Header().Add("X-Resp-A6-Runner", "Go")
+                _, err = res.Write(middlewareResponseBodyBytes)
+                if err != nil {
+                        log.Errorf("failed to write %s", err)
+                }
+	}
+}
+
+func (p *Firetail) ResponseFilter(conf interface{}, res pkgHTTP.Response) {
+        firetailMiddleware, err := firetail.GetMiddleware(&firetail.Options{
+                OpenapiSpecPath:          "./appspec.yml",
+                LogsApiToken:             "",
+                LogsApiUrl:               "",
+                DebugErrs:                true,
+                EnableRequestValidation:  false,
+                EnableResponseValidation: true,
+        })
+
+        if err != nil {
+                log.Errorf("Failed to initialise Firetail middleware, err:", err.Error())
+        }
+
+
+	body, err := res.ReadBody()
+
+        // Create a fake handler
+        myHandler := &stubHandler{
+                responseCode:  res.StatusCode(),
+                responseBytes: body,
+        }
+
+	// Create our middleware instance with the stub handler
+	myMiddleware := firetailMiddleware(myHandler)
+
+	// Create a local response writer to record what the middleware says we should respond with
+	localResponseWriter := httptest.NewRecorder()
+
+	// Serve the request to the middlware
+	myMiddleware.ServeHTTP(localResponseWriter, httptest.NewRequest(
+		"GET", "/get",
+                io.NopCloser(bytes.NewBuffer([]byte{})),
+	))
+
+	middlewareResponseBodyBytes, err := io.ReadAll(localResponseWriter.Body)
+
+	if err != nil {
+		log.Errorf("Failed to read response body bytes from middleware, err:", err.Error())
+
+                _, err = res.Write(middlewareResponseBodyBytes)
+                if err != nil {
+                        log.Errorf("failed to write %s", err)
+                }
+	}
+
+	if localResponseWriter.Code != res.StatusCode() {
+		log.Errorf("Middleware altered status code from %d to %d", res.StatusCode, localResponseWriter.Code)
+
+                _, err = res.Write(middlewareResponseBodyBytes)
+                if err != nil {
+                        log.Errorf("failed to write %s", err)
+		}
+	}
+
+	if string(middlewareResponseBodyBytes) != string(body) {
+		log.Errorf("Middleware altered response body, original: %s, new: %s", string(body), string(middlewareResponseBodyBytes))
+
+                _, err = res.Write(middlewareResponseBodyBytes)
+                if err != nil {
+                        log.Errorf("failed to write %s", err)
+                }
 	}
 }
 
