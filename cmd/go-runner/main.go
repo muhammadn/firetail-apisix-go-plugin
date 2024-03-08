ffe4296f1aae78e4c8a838378f2ed1419f67c0a3
Muhammad Nuzaihan Bin Kamal Luddin <muhammad@taqisystems.com>
2024-03-09 00:35:03 +0800

    finish up code and refactor unnecessary files

--- cmd/go-runner/main.go
+++ cmd/go-runner/main.go
@@ -9,8 +9,8 @@ import (
 
 
         _ "firetail_apisix_plugin/cmd/go-runner/plugins"
-	"firetail_apisix_plugin/pkg/runner"
-	"firetail_apisix_plugin/pkg/log"
+       "github.com/apache/apisix-go-plugin-runner/pkg/runner"
+       "github.com/apache/apisix-go-plugin-runner/pkg/log"
 )
 
 const (
